43d9b43ea811865a88c42c551365091a0d80b937
ealkilicgil <ealkilicgil@gmail.com>
2019-06-02 11:33:00 +0300

    Migratiıon added

--- CleanArch.Mvc/Data/Migrations/20190602081716_InitialMigration.Designer.cs
+++ CleanArch.Mvc/Data/Migrations/20190602081716_InitialMigration.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using CleanArch.Mvc.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CleanArch.Mvc.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190602081716_InitialMigration")]
+    partial class InitialMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
